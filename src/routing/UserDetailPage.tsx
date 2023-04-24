2234667b47a64ba515d814537eacc51abc41bec2
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-24 20:17:06 +0700

    Pass & Get data from route parameters

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,10 @@
+import { useLocation, useParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams();
+  const location = useLocation();
+  console.log(params, location);
+
   return <p>User</p>;
 };
 
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
-import HomePage from "./HomePage";
-import UserListPage from "./UserListPage";
 import ContactPage from "./ContactPage";
+import HomePage from "./HomePage";
+import UserDetailPage from "./UserDetailPage";
+import UserListPage from "./UserListPage";
 
 const router = createBrowserRouter([
   { path: "/", element: <HomePage /> },
   { path: "/users", element: <UserListPage /> },
+  { path: "/users/:id", element: <UserDetailPage /> },
   { path: "/contacts", element: <ContactPage /> },
 ]);
 
