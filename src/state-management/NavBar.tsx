0c96011b6c00c4956d873f206ddfc7aa05e5a442
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-24 18:06:03 +0700

    Only re-renders components with specific state property

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,11 +1,10 @@
-import { useContext } from "react";
-import TasksContext from "./tasks/taskContext";
 import { LoginStatus } from "./auth";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
-  const { tasks } = useContext(TasksContext);
-  const { counter } = useCounterStore();
+  const counter = useCounterStore((s) => s.counter);
+
+  console.log("Render NavBar");
 
   return (
     <nav className="navbar d-flex justify-content-between">
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -2,14 +2,16 @@ import { create } from "zustand"
 
 interface CounterStore {
   counter: number
+  max: number
   increment: () => void
   reset: () => void
 }
 
 const useCounterStore = create<CounterStore>(set => ({
   counter: 0,
+  max: 5,
   increment: () => set(store => ({ counter: store.counter + 1 })),
-  reset: () => set(store => ({ counter: 0 }))
+  reset: () => set(store => ({ max: 10 }))
 }))
 
 export default useCounterStore
