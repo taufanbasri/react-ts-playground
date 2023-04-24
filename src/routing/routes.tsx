4a38477178391d812375efba7492355e8ec48617
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-25 03:13:35 +0700

    Nested Routes

--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -3,12 +3,19 @@ import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  { path: "/users/:id", element: <UserDetailPage /> },
-  { path: "/contacts", element: <ContactPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { index: true, element: <HomePage /> },
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+      { path: "contacts", element: <ContactPage /> },
+    ],
+  },
 ]);
 
 export default router;
