eba4737009acb2ea1cfaacf52b7aba42117b81c1
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-24 17:46:34 +0700

    Manage state with Zustand

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import TasksContext from "./tasks/taskContext";
 import { LoginStatus } from "./auth";
+import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   const { tasks } = useContext(TasksContext);
+  const { counter } = useCounterStore();
 
   return (
     <nav className="navbar d-flex justify-content-between">
-      <span className="badge text-bg-secondary">{tasks.length}</span>
+      <span className="badge text-bg-secondary">{counter}</span>
       <LoginStatus />
     </nav>
   );
